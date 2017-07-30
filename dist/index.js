3ab2efd44950d67e9710ee09b429ba9cfb5d3c74
steady-daddy <thisismansoorkhan@gmail.com>
2017-07-30 16:06:18 +0530

    Using JSX and transpiling with babel for testing purposes only

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,13 @@
-const { createElement } = React
 const { render } = ReactDOM
 
-const style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Verdana'
-}
-const title = createElement(
-    'h1',
-    {id: 'title', className: 'header', style: style},
-    'Hello World'
-)
 render(
-    title,
+    <h1 id='title'
+        className='header'
+        style={{backgroundColor: 'orange',
+                color: 'white',
+                fontFamily: 'Verdana'}}
+    >
+        Hello World.
+    </h1>,
     document.getElementById('react-container')
 )
